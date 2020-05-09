2d1fd81134cb506863111d735bfa5d6b07cd31da
thienphamstu <52187201+0356581777@users.noreply.github.com>
2020-05-09 21:37:20 +0700

    add

---
+++ resources/views/backend/loginadmin.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('backend/css/bootstrap.min.css') }}">
+    <script src="{{ asset('backend/js/bootstrap.min.js') }}">
+
+    </script>
+    <script src="{{ asset('backend/js/jquery-3.4.1.slim.min.js') }}">
+
+    </script>
+    <script src="{{ asset('backend/js/popper.min.js') }}">
+
+    </script>
+<body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    
+                </div>
+            </div>
+        </div>
+</body>
+</html>
