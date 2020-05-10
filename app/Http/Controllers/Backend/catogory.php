0e82d7d4cf6fdd2d9420e969a77f1856a4fb4220
thienphamstu <52187201+0356581777@users.noreply.github.com>
2020-05-10 13:29:45 +0700

    update

--- app/Http/Controllers/Backend/catogory.php
+++ app/Http/Controllers/Backend/catogory.php
@@ -11,18 +11,31 @@ class catogory extends Controller
     //
     function catogory()
     {
-        $list_cat=db_category::where('db_category.status','=',0)
+        $list_cat=db_category::where('db_category.status','=',1)
         ->join('db_user','db_category.created_by','=','db_user.id')->select('db_category.*','db_user.fullname as ten')
         ->get();
+       
        
         return view('backend.category',compact('list_cat'));
     }
     function update_category($id_loai)
     {
         $itemloai=db_category::find($id_loai);
+        if($itemloai != null)
+        {
         $itemloai->status=0;
-        $itemloai->save();
+        if($itemloai->save())
+            {
+                $status=1;
 
-        return response($itemloai);
+                return response($status);
+            }
+
+        }
+        else
+        {
+            $status=0;
+            return response($status);
+        }
     }
 }
