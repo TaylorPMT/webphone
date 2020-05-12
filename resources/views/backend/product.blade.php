4c56f9acbed0a7188c821ce5bdd9cf6300cff917
unknown <you@example.com>
2020-05-12 02:54:54 -0700

    done-product

--- resources/views/backend/product.blade.php
+++ resources/views/backend/product.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 
   <h2>Quản Lý Sản Phẩm</h2>
-         
+  <span>@includeIf('backend.modules.message')</span>     
   <table class="table table-dark table-striped" id="myTable">
     <thead>
       <tr>
@@ -27,20 +27,31 @@
           <td>{{$product->price}}</td>
           <td>{{$product->pricesale == 0 ? "Không có khuyến mãi":$product->pricesale}}</td>
           <td>{{$product->created_at}}</td>
-          <td>{{$product->status=1?"On":"Off"}}</td>
+          <td>
+          @php
+              if($product->status !=1)
+              {
+                echo "<span class='text-danger'>Off</span>";
+              }else
+              {
+                echo "<span class='text-success'>On</span>";
+              }
+          @endphp
+
+          </td>
           <td>
             <!-- Xóa -->
-              <a href="#">
+              <a href="{{Route('getXoa',['id'=>$product->id])}}" style="padding:0 5px">  
                 <i class="far fa-trash-alt"></i>
-                <span>Xóa</span>
+                  <span>Xóa</span>  
               </a>
             <!-- Sữa -->
-              <a href="#">
+              <a href="{{Route('getSua',['id'=>$product->id])}}" style="padding:0 5px">
                 <i class="fas fa-tools"></i>
                 <span>Sữa</span>
               </a>
-              <!-- Trạng Thái -->
-              <a href="#">
+            <!-- Trạng Thái -->      
+              <a href="{{Route('status',['id'=>$product->id])}}" style="padding:0 5px">
                 <i class="fas fa-exchange-alt"></i>
               </a>
           </td>
@@ -48,5 +59,11 @@
         @endforeach
     </tbody>
   </table>
+  <script>
+    $(document).ready( function () {
+      $('#myTable').DataTable();
+  } );
+  </script>
   @endsection
+  
 
